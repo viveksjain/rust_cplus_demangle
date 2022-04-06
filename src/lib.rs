4a1cf90c20543ddc955ffab8e8b4dffa2af49ae8
Vivek Jain <vivek.jain@rubrik.com>
2022-04-05 17:01:24 -0700

    Minor cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,4 @@
-//! # cplus_demangle
-//! [![crates.io](https://img.shields.io/crates/v/cplus_demangle)](https://crates.io/crates/cplus_demangle) [![docs.rs](https://docs.rs/cplus_demangle/badge.svg)](https://docs.rs/cplus_demangle/latest/cplus_demangle/)
-//!
-//! This library converts C++ mangled symbol names to human-readable strings. It is a safe Rust wrapper to GNU libiberty's C function `cplus_demangle`.
-//!
-//! ## Example
-//! Suppose you compile the following C++ program:
-//! ```cpp
-//! namespace test {
-//!   void myfn(int x) { }
-//! }
-//! ```
-//!
-//! In the resulting binary, the symbol that gets generated for `myfn` is `_ZN4test4myfnEi`. We can convert it back with this Rust code:
-//! ```rust
-//! assert_eq!(cplus_demangle::demangle("_ZN4test4myfnEi").unwrap(), "test::myfn(int)");
-//! ```
+#![doc = include_str!("../README.md")]
 
 use libc::{c_char, c_int};
 use std::ffi::{CStr, CString};
